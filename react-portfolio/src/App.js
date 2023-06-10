069fefa2a051fae778cac326a5f08d0f5af39ed3
Mandy Delmaine <mandydelmaine@gmail.com>
2023-06-10 13:01:11 -0400

    initial commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AboutMe from './components/pages/AboutMe';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main>
+        <AboutMe name="Mandy" />
+      </main>
+    </div>
+  );
+}
---
+++ react-portfolio/src/components/pages/PortfolioContainer.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // TODO: Add a comment describing the functionality of this method
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Resume />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
