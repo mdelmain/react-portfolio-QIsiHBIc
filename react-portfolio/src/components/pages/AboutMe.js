d9c1522f45482cbaa3e0ff517a99dfce0e0539e3
Mandy Delmaine <mandydelmaine@gmail.com>
2023-06-21 20:27:17 -0400

    adjust image paths

--- react-portfolio/src/components/pages/AboutMe.js
+++ react-portfolio/src/components/pages/AboutMe.js
@@ -7,7 +7,7 @@ export default function AboutMe() {
     return (
         <>
           <h1>About Me</h1>
-          <img src={"/images/github-profile.png"} alt={"profile"} height={100} width={100} style={image}/>
+          <img src={"./images/github-profile.png"} alt={"profile"} height={100} width={100} style={image}/>
           <p>
           I am a full-stack bootcamp student who is learning the fundamentals of JavaScript, CSS, and HTML. My GitHub profile with samples of my work can be found here: https://github.com/mdelmain
           </p>
--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -17,42 +17,42 @@ export default function Portfolio() {
       <h1>Portfolio</h1>
       <div style={container}>
         <ProjectCard
-          image="/images/password.png"
+          image="./images/password.png"
           title="Password Generator"
           text="JavaScript"
           siteLink="https://mdelmain.github.io/password-generator-lTOcqbAD/"
           githubLink="https://github.com/mdelmain/password-generator-lTOcqbAD"
         />
         <ProjectCard
-          image="/images/weather4.png"
+          image="./images/weather4.png"
           title="Weather Dashboard"
           text="JavaScript"
           siteLink="https://mdelmain.github.io/weather-dashboard-sISvhpsH/"
           githubLink="https://github.com/mdelmain/weather-dashboard-sISvhpsH"
         />
         <ProjectCard
-          image="/images/regex.png"
+          image="./images/regex.png"
           title="Regex Tutorial"
           text="Regex"
           siteLink="https://gist.github.com/mdelmain/44c674201595e4d7bee5b66bc303b3ec"
           githubLink="https://gist.github.com/mdelmain/44c674201595e4d7bee5b66bc303b3ec"
         />
         <ProjectCard
-          image="/images/orm.png"
+          image="./images/orm.png"
           title="Object-Relational-Mapping"
           text="JavaScript, MYSQL"
           siteLink="https://drive.google.com/file/d/1gwiRXqaJVjnSc-nXhSeg8-LZm2XFGVDu/view"
           githubLink="https://github.com/mdelmain/ORM-MpgwZmJL"
         />
         <ProjectCard
-          image="/images/employee-tracker-2.png"
+          image="./images/employee-tracker-2.png"
           title="Employee Tracker"
           text="JavaScript, MYSQL"
           siteLink="https://drive.google.com/file/d/1lmHHUdttrpCwnjJ6D9-raFWV9wHvpCvH/view"
           githubLink="#https://github.com/mdelmain/employee-tracker-vlfHrCLC"
         />
         <ProjectCard
-          image="/images/svg.png"
+          image="./images/svg.png"
           title="SVG Logo Maker"
           text="JavaScript"
           siteLink="https://drive.google.com/file/d/1VXII5r-Szw5PaaXYeyOzPzqxXsSnuqpg/view"
